18fa31ae401dc3844b8711d82698b39a6faa8788
Gaspare Ferraro <ferraro@gaspa.re>
2020-09-02 18:36:37 +0200

    final

---
+++ 1-Assignment/Ex1/Ex1.2/TestingAlgorithmsPlus/src/annot/Encrypt.java
@@ -0,0 +1,23 @@
+package test;
+
+/*
+  Exam: Advance Programming
+  Year: 2019/2020
+  Author: Gaspare Ferraro
+  Mail: ferraro@gaspa.re
+  Mat: 520549
+*/
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+
+import java.lang.annotation.Annotation;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface Encrypt {
+
+}
